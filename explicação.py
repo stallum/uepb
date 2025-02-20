0e02825c1b84b71c5b4ba1e110ab28101ca5f0b4
unknown <arturgab008@gmail.com>
2025-02-20 16:54:18 -0300

    alteração de arquivos

---
+++ explicação.py
@@ -0,0 +1,33 @@
+print('Hello World')
+print(3)
+print(4.9)
+print(3 + 4j )
+print(type(3 + 4j))
+print(type(4.9))
+
+msg = "Alô Mundo"
+idade = 4
+altura = 1.8
+
+print(msg)
+
+nota1 = float(input('Digite aqui sua primeira nota: '))
+nota2 = float(input('Digite aqui sua segunda nota:  '))
+media = (nota1 + nota2)/2
+
+print(f'a média de suas notas é {media:.2f}')
+
+
+entrada = input()
+print('o nome informado foi:', entrada)
+print('o nome informado foi:'+ entrada) # o + significa além de somar contatenar
+
+try: 
+    nota1 = float(input('Digite aqui sua primeira nota: '))
+    nota2 = float(input('Digite aqui sua segunda nota:  '))
+    media = (nota1 + nota2)/2
+
+    print(f'a média de suas notas é {media:.2f}')
+except ValueError: 
+    print('Isso não é um número, por favor escreva um número')
+
