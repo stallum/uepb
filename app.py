344222e33c9d4a44062dd72734e2762c992ea91e
unknown <arturgab008@gmail.com>
2025-02-20 16:33:09 -0300

    explicação um

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 print('Hello World')
+print(3)
+print(4.9)
+print(3 + 4j )
+print(type(3 + 4j))
+print(type(4.9))
+
+msg = "Alô Mundo"
+idade = 4
+altura = 1.8
+
+print(msg)
+
+nota1 = float(input('Digite aqui sua primeira nota: '))
+nota2 = float(input('Digite aqui sua segunda nota:  '))
+media = (n1 + n2)/2
+
+print(f'a média de suas notas é {media}')
