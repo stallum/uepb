c5b10f7f1884dd36a3159b135c7471157c390ca1
Artur Gabriel <arturgab008@gmail.com>
2025-02-25 18:23:26 -0300

    exercícios resolvidos

---
+++ Kézia aula python/exercicios/ex19.py
@@ -0,0 +1,7 @@
+# faça um programa que calcula o tempo para passar um arquivo com o tamanho do arquivo e a velocidade da internet
+# formula: v = arq * t (v * arq = t)
+
+arq = float(input('Digite aqui o tamanho do arquivo que você quer transferir (Apenas números em KBs): '))
+velocidade = float(input('Digite aqui a velocidade da sua internet (Apenas números KB/s )'))
+
+print(f'Os arquivos serão enviados em: {arq * velocidade}')
