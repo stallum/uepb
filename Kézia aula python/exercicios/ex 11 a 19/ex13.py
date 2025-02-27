052407110fd1f46e97488842b3072eacc878aae5
unknown <arturgab008@gmail.com>
2025-02-27 17:14:20 -0300

    exercicio 13

--- Kézia aula python/exercicios/ex 11 a 19/ex13.py
+++ Kézia aula python/exercicios/ex 11 a 19/ex13.py
@@ -1,1 +1,18 @@
 # 13.	Faça um algoritmo que receba o preço de custo de um produto e mostre o valor de venda. Sabe-se que o preço de custo receberá um acréscimo de acordo com um percentual informado pelo usuário.
+# valorVenda = custo * percent
+
+def percentVal():
+    while True:
+        try:
+            percent = float(input(' use apenas o numero sem ser dividido por 100 \n exemplo: 30 para 30%/0.3 \n Digite um valor percentual que será adicionado ao produto: '))
+            percent = percent/100
+            return percent
+        except ValueError:
+            print('Isso não é um número válido')
+
+custo = float(input(f'Digite o valor de custo do produto a ser vendido: '))
+if custo <= 0:
+    print('Esse não é um valor válido!')
+else: 
+    valorVenda = custo * (1 + percentVal())
+    print(f'O valor do produto final em cima do custo do produto e o percentual informado deve ser: {valorVenda}')
