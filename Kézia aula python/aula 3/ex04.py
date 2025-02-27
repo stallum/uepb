87e649e2c5651f0d47888c6bf119c08a3f101796
unknown <arturgab008@gmail.com>
2025-02-27 17:50:04 -0300

    aula kezia final

--- Kézia aula python/aula 3/ex04.py
+++ Kézia aula python/aula 3/ex04.py
@@ -1,9 +1,21 @@
 # um programa que receba um x como entrada e devolva uma aproximação do arco tangente de x
 x = float(input('Digite um número real: '))
 result = 0
-for i in range(0, 51, 2):
-    term = ((-1)) ** i * (x ** (2 * i + 1)) / (2 * i + 1)
+termos = 50
+for i in range(termos):
+    term = ((-1) ** i) * (x ** (2 * i + 1)) / (2 * i + 1)
     result += term
 
 aproximation = result
 print(f'A aproximação de arctan({x}) com 50 termos: {aproximation:.2f}')
+
+expoente = 1
+sinal = 1
+arctan = 0
+x = float(input('Digite um número real: '))
+for i in range(50):
+    arctan += sinal * ((x**expoente)/expoente)
+    expoente += 2
+    sinal *= -1 
+print('Kezia')
+print(f'arctan de {x} é: {arctan}')
