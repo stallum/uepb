613c143c739c60d4c899b2f0c8270671b766db4a
unknown <arturgab008@gmail.com>
2025-02-27 16:57:53 -0300

    alterações em pastas

---
+++ Kézia aula python/exercicios/ex 20 a 29/ex22.py
@@ -0,0 +1,22 @@
+# Pedir dois números e imprima o maior deles
+'''num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+if num1 > num2:
+    print(f'O número {num1} é maior que o número {num2}')
+else:
+    print(f'O número {num2} é maior que o número {num1}')'''
+
+#
+
+def numeroValido(indice):
+    while True:
+        try:
+            num = float(input(f'Digite o {indice + 1}º numero: '))
+            return num 
+
+        except ValueError: 
+            print('isso são é um número válido')
+
+numero = [numeroValido(i) for i in range(2)]
+soma = sum(numero)
+print(f'A soma desses dois números é: {soma:.2f}')
--- Kézia aula python/exercicios/ex 22 a 45/ex22.py
+++
@@ -1,7 +0,0 @@
-# Pedir dois números e imprima o maior deles
-num1 = float(input('Digite um número: '))
-num2 = float(input('Digite outro número: '))
-if num1 > num2:
-    print(f'O número {num1} é maior que o número {num2}')
-else:
-    print(f'O número {num2} é maior que o número {num1}')
