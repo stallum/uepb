9fffa38a8ca0d1cbc3ad9d957d88839de87aa741
Artur Gabriel <stallumg8@gmail.com>
2025-03-10 15:07:24 -0300

    adição atividade

---
+++ Kézia aula python/exercicios/ex 60 a 69/ex63.py
@@ -0,0 +1,15 @@
+'''
+faça um programa que receba o valor de uma dívida e mostre uma table com o segueintes dados: valor da dívida, valor dos juros, quantidade de parcelas e valor da parcela.
+Os juros e a quantidade de parcelas seguem a tabela abaixo: 
+Quantidade de Parcelas  % de Juros sobre o valor inicial da dívida
+1       0
+3       10
+6       15
+
+Exemplo de saída do programa:
+Valor da Dívida Valor dos Juros Quantidade de Parcelas  Valor da Parcela
+R$ 1.000,00     0               1                       R$ 1.000,00
+R$ 1.100,00     100             3                       R$ 366,00
+R$ 1.150,00     150             6                       R$ 191,67
+'''
+
