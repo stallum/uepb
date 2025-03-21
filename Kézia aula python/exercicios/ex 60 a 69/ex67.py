1df011bd4890694da65ffcb6d4cc349d746bb38e
Artur Gabriel <stallumgp8@gmail.com>
2025-03-21 09:13:34 -0300

    criando novas solução para os códigos

--- Kézia aula python/exercicios/ex 60 a 69/ex67.py
+++ Kézia aula python/exercicios/ex 60 a 69/ex67.py
@@ -11,7 +11,117 @@ Refrigerante          105     	R$ 1,00
 Faça um programa que leia o código dos itens pedidos e as quantidades desejadas. Calcule e mostre o valor a ser pago por item (preço * quantidade) e o total geral do pedido. Considere que o cliente deve informar quando o pedido deve ser encerrado. 
 """
 import pandas as pd
+itens = ['Cachorro Quente', 'Bauru Simple', 'Bauru com ovo', 'Hambúrguer', 'Cheeseburguer', 'Refrigerante']
+codigos = [100, 101, 102, 103, 104, 105]
+preços = [1.20, 1.30, 1.50, 1.20, 1.30, 1.00]
 
+cardapio_display = {
+    'Itens': itens,
+    'Código': codigos,
+    'Preço': preços
+}
+
+cardapio_df = pd.DataFrame(cardapio_display)
+
+pedido = 0
+total_pedido = 0
+quantidade_txt = 'Digite a quantidade que você quer: '
+
+while pedido == 0:
+    print(cardapio_df)
+    codigo = input('Digite um código presente em nosso cardápio, (ou "0" para finalizar o pedido): ')
+    while codigo.isalpha():
+        codigo = input('Código inválido, digite um código em nosso cardápio: ')
+    
+    codigo = int(codigo)
+
+    if codigo == 0:
+        pedido += 1
+
+    
+    elif codigo < 100 or codigo > 105:
+        while int(codigo) < 100 or int(codigo) > 105:
+            print('código inválido')
+            print(cardapio_df)
+            codigo = input('digite um dos códigos presentes no nosso cardápio: ')
+        codigo = int(codigo)
+            
+        if codigo == 100:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Cachorro Quente' 
+            total = 1.2 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total
+
+        elif codigo == 101:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Bauru Simples'
+            total = 1.3 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total 
+                    
+        elif codigo == 102:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Bauru com ovo'
+            total = 1.5 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total
+
+        elif codigo == 103:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Hambúrguer'
+            total = 1.2 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total
+                         
+        elif codigo == 104:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Cheeseburguer'
+            total = 1.3 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total
+                            
+
+        elif codigo == 105:
+            quantidade = input(quantidade_txt)
+            while quantidade.isalpha() or int(quantidade) < 0:
+                quantidade = input('quantidade inválida, digite uma quantidade válida: ')
+            quantidade = int(quantidade)
+
+            nome = 'Refrigerante'
+            total = 1.0 * quantidade
+            print(f"{nome} ({quantidade}x) - R$ {total:.2f}")
+            total_pedido += total
+
+print(f'\n total geral do pedido: R${total_pedido:.2f}')
+
+
+
+#
+
+
+
+'''
 itens = ['Cachorro Quente', 'Bauru Simple', 'Bauru com ovo', 'Hambúrguer', 'Cheeseburguer', 'Refrigerante']
 codigos = [100, 101, 102, 103, 104, 105]
 preços = [1.20, 1.30, 1.50, 1.20, 1.30, 1.00]
@@ -40,7 +150,7 @@ itens_pedido = []
 
 print(cardapio_df)
 while pedido == 0:
-    codigo = input('Digite o código de um produto de nosso cardápio: ')
+    codigo = input('Digite o código de um produto de nosso cardápio: (ou zero, caso queira terminar o pedido)')
     
     if int(codigo) == 0:
         pedido += 1
@@ -67,3 +177,4 @@ for item in itens_pedido:
     print(f'{nome} ({quantidade}x) - R$ {total:.2f}')
   
 print(f"\nTotal geral do pedido: R$ {total_pedido:.2f}")
+'''
--- Kézia aula python/exercicios/ex 60 a 69/ex70.py
+++ Kézia aula python/exercicios/ex 60 a 69/ex70.py
@@ -1,10 +1,17 @@
 '''
 27.	Faça um programa para validar o login e a senha de um usuário. Caso o usuário informe algum valor inválido informar o erro e pedir novamente os dados. A leitura dos dados deve ser encerrada quando o usuário digitar 3 vezes um valor inválido (login ou senha). Considere o login válido como "kezia" e a senha como "123".
 '''
+count = 1
+
 login = input('Digite o Login: ').lower()
-while login != 'kezia':
+while count < 3 and login != 'kezia':
     login = input('Login inválido, digite um login válido: ').lower()
+    count += 1
 
 senha = input('Digite sua senha: ')
-while senha != '123':
+while count < 3 and senha != '123':
     senha = input('Senha inválida, digite sua senha novamente: ')
+    count += 1
+
+
+print(f'ERRO, VOCÊ EXCEDEU AS TENTATIVAS DE LOGIN')
